730f37fe1b8e00fdf2414fa31fc725b9cb8253cf
Aom <radtanaporn.kn@gmail.com>
2018-10-21 18:26:23 +0700

    test_again

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ app.get('/product_delete/:pid', function (req, res) {
 app.get('/users', function (req, res) {
     var id = req.param('id');
     
-    var sql = 'select* from users order by ASC';
+    var sql = 'select* from users';
     if (id) {
         sql += ' Where id =' + id;
     }
